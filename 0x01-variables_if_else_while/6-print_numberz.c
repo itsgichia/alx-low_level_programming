6646772ca701d53a2d59a8e0460aafcb2ceddb1c
itsgichia <gichiamuiruri@gmail.com>
2023-02-18 13:12:29 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success(
+ */
+int main(void)
+{
+	int s = 0, e = 9
+
+	while (s <= e)
+	{
+		putchar(s + '0');
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
