e134417a26dea18199575067bd7ac322dd8f838d
itsgichia <gichiamuiruri@gmail.com>
2023-02-23 01:55:41 -0800

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,6 +3,8 @@
 /**
  * _isupper - function that checks for uppercase character
  *
+ * @c: character to be checked
+ *
  * Return: 1 if character is uppercase, otherwise 0
  */
 int _isupper(int c);
