609414eb18dfe6c13a85edf836888cd3b3ca39c4
itsgichia <gichiamuiruri@gmail.com>
2023-02-23 13:25:20 -0800

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,7 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 0)
 	{
@@ -17,8 +17,18 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar('\\');
+			for (j = 0; j <= i; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
 		}
 	}
-	_putchar('\n');
 }
