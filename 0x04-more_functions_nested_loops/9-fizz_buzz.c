a980db10ea0bd6619c8e45774b54cb5145fe456f
itsgichia <gichiamuiruri@gmail.com>
2023-02-24 01:17:29 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 /**
- * main - fizz ball test
+ * main - fizz buzz test
  *
  * Return: 0 on success
  */
 int main(void)
 {
-	int i;
+        int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0  && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
-	}
+        for (i = 1; i <= 100; i++)
+        {
+                if (i % 3 == 0  && i % 5 == 0)
+                {
+                        printf("FizzBuzz ");
+                }
+                else if (i % 3 == 0)
+                {
+                        printf("Fizz ");
+                }
+                else if (i % 5 == 0)
+                {
+                        printf("Buzz ");
+                }
+                else
+                {
+                        printf("%d ", i);
+                }
+        }
 
-	printf("\n");
+        printf("\n");
 
-	return (0);
+        return (0);
 }
